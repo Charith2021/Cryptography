0832cda91a17c99021cac57fdaafc05410b9f23a
Charith Gimhana <charithgimhana97@gmail.com>
2021-05-24 09:58:45 +0530

    Third commit

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,2 +1,9 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class MainFormController {
+    public Button btnEncrypt;
+
+    public void btnEncryptOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/SecondFormController.java
+++ src/SecondFormController.java
@@ -1,2 +1,9 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class SecondFormController {
+    public Button btnDecrypt;
+
+    public  btnDecryptOnAction(ActionEvent actionEvent) {
+    }
 }
